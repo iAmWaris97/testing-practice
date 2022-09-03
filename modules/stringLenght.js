484c7a3ebcd47132b16f0ee33113089b84b81de8
Waris Haleem <43011742+iAmWaris97@users.noreply.github.com>
2022-09-03 17:53:26 +0430

    Test stringLenght func for its lenght range!

--- modules/stringLenght.js
+++ modules/stringLenght.js
@@ -1,4 +1,5 @@
 function stringLength(string) {
+  if (string.length < 1 || string.length > 10) return 'String lengh is not in the expected range (1 to 10 characters long';
   return string.length;
 }
 
--- string.test.js
+++ string.test.js
@@ -3,3 +3,11 @@ const stringLength = require('./modules/stringLenght');
 test('stringLength function should return the length of the string', () => {
   expect(stringLength("Waris")).toBe(5);
 });
+
+test('String lenght should be between 1 to 10 characters', () => {
+  expect(stringLength('')).toBe('String lengh is not in the expected range (1 to 10 characters long');
+});
+
+test('String lenght should be between 1 to 10 characters', () => {
+  expect(stringLength('Waris Haleem not Shams Haleem')).toBe('String lengh is not in the expected range (1 to 10 characters long');
+});
